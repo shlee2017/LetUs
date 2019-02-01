df8b17386b52dee45170fd0ff5cf59f58a5ea76b
leesnap <leesnap@umich.edu>
2019-02-01 12:24:20 -0500

    initial commit comment

---
+++ LetUs/CustomTabBar.swift
@@ -0,0 +1,32 @@
+//
+//  CustomTabBar.swift
+//  LetUs
+//
+//  Created by Sangheon Lee on 2/1/19.
+//  Copyright © 2019 Ethan Lee. All rights reserved.
+//
+
+import UIKit
+
+class CustomTabBar: UITabBarController {
+
+    var tabBarItem = UITabBarItem()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
