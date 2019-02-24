c9a6cb0a15ac584cb33e54e36592a6680aebf64b
gigigarate <agarate@umich.edu>
2019-02-24 15:08:30 -0500

    added aws framework

--- LetUs/Constants.swift
+++ LetUs/Constants.swift
@@ -7,3 +7,11 @@
 //
 
 import Foundation
+import AWSCognitoIdentityProvider
+
+let CognitoIdentityUserPoolRegion: AWSRegionType = .Unknown
+let CognitoIdentityUserPoolId = "YOUR_USER_POOL_ID"
+let CognitoIdentityUserPoolAppClientId = "YOUR_APP_CLIENT_ID"
+let CognitoIdentityUserPoolAppClientSecret = "YOUR_APP_CLIENT_SECRET"
+
+let AWSCognitoUserPoolsSignInProviderKey = "UserPool"
