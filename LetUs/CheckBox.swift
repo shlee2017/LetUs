e8686fc20035b699d7a67107549b3a84a2c87000
seanlu99 <seanlu99@gmail.com>
2019-03-31 21:28:46 -0400

    Checkboxes added

---
+++ LetUs/CheckBox.swift
@@ -0,0 +1,37 @@
+//
+//  CheckBox.swift
+//  LetUs
+//
+//  Created by Sean Lu on 3/31/19.
+//  Copyright © 2019 Ethan Lee. All rights reserved.
+//
+
+import UIKit
+
+class CheckBox: UIButton {
+    // Images
+    let checkedImage = UIImage(named: "ic_check_box")! as UIImage
+    let uncheckedImage = UIImage(named: "ic_check_box_outline_blank")! as UIImage
+    
+    // Bool property
+    var isChecked: Bool = false {
+        didSet{
+            if isChecked == true {
+                self.setImage(checkedImage, for: UIControl.State.normal)
+            } else {
+                self.setImage(uncheckedImage, for: UIControl.State.normal)
+            }
+        }
+    }
+    
+    override func awakeFromNib() {
+        self.addTarget(self, action:#selector(buttonClicked(sender:)), for: UIControl.Event.touchUpInside)
+        self.isChecked = false
+    }
+    
+    @objc func buttonClicked(sender: UIButton) {
+        if sender == self {
+            isChecked = !isChecked
+        }
+    }
+}
